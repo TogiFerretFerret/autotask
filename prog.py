1dab7faf6bc71a0fac3998bf75e80fc972819f46
TogiFerretFerret <riverdalecoder@outlook.com>
2025-02-10 16:44:03 -0500

    +kappa

--- prog.py
+++ prog.py
@@ -1,12 +1,3 @@
 import pyautogui
-import time
-from text_finder import find_text_coordinates
+pyautogui.write("hello world")
 
-subject_coords = find_text_coordinates("Subject")
-saved_subject_coords = subject_coords
-pyautogui.click(saved_subject_coords[0]+50, saved_subject_coords[1])
-pyautogui.write("Click recognition")
-time.sleep(0.5)
-
-pyautogui.click(saved_subject_coords[0], saved_subject_coords[1] + 30)
-pyautogui.write("Hi Tahlioubeah, I got click recognition to work through a python script that utilizes a library to find text on the screen and click those coordinates.")
